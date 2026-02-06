71a57687389fa69b37e84a2543d501670f4f49c5
AlliePinnell <x-rap@hotmail.ca>
2026-02-06 15:27:33 -0500

    question 8 wip

--- Questions/Q8/VS/question_8/question_8/question_8.cpp
+++ Questions/Q8/VS/question_8/question_8/question_8.cpp
@@ -1,30 +1,58 @@
 #include <iostream>
+#include <vector>
+using std::vector;
 
 struct node {
     int data;
-    node* next;
+    struct node* next;
+
+    node(int new_data) {
+        data = new_data;
+        next = nullptr;
+    }
 };
 
+node* newTop = nullptr;
 node* top = nullptr;
 node* bottom = nullptr;
 
+node* topLL = nullptr;
+vector<int> arr;
 
-void badPop() {
-    e = top->data;
-    tp = bottom;
-    while (tp->next != top)
-    {
+int arrTop;
+
+//void badPop() {
+//    e = top->data;
+//    tp = bottom;
+//    while (tp->next != top)
+//    {
+//        tp = tp->next;
+//    }
+//    top = tp;
+//    delete tp->next;
+//    tp->next = nullptr;
+//    return e;
+//}
+
+//void badPush() {
+//    for (i = bottom; i > 0; i--) {
+//        s[i + 1] = s[i];
+//    }
+//    bottom++;
+//    s[0] = e;
+//}
+
+void outputSinglyLinkedList(node*& head) {
+    node* tp = head;
+    while (tp) {
+        std::cout << tp->data << " -> ";
         tp = tp->next;
     }
-    top = tp;
-    delete tp->next;
-    tp->next = nullptr;
-    return e;
 }
 
-void goodWay() {
+int PopLL() {
     if (top == nullptr) {
-        return;
+        return 0;
     }
 
     node* tp = top;
@@ -33,12 +61,80 @@ void goodWay() {
     delete tp;
     return e;
 }
+void PushLL(int e) {
+    newTop = new node(e);
+    newTop->next = top;
+    top = newTop;
+}
+
+
+int PopArray() {
+    int t = arrTop;
+    arr[arrTop] = 0;
+    arrTop--;
+
+    /* this stuff has gotta go in the final*/
+    if (!(t > 10)) {
+        std::cout << "Popped: " << t << "\n";
+    }  
+
+    return arrTop;
+}
+void PushArray(int e) {
+    arr[arrTop + 1] = e;
+    arrTop++;
+}
+
 
 int main()
 {
 	int sizes[] = { 10, 100, 1000, 10000, 100000 };
 
 	for (int n : sizes) {
+        arrTop = -1;
+        arr.resize(n);
+
+        for (int i = 0; i < n; ++i) {
+            //PushLL(i);
+            PushArray(i);
+        }
+
+        std::cout << "Pushed " << n << " elements | array size = " << arr.size();
+        std::cout << "\n";
+
+        arrTop = n - 1;
+
+        for (int i = 0; i < n; ++i) {
+            PopLL();
+            PopArray();
+        }
+
+        std::cout << "Stack empty after popping all elements.\n\n";
 
 	}
 }
+
+//for (int n : sizes) {
+//    // Reset stacks for each benchmark size
+//    // Clear linked list
+//    while (top) {
+//        goodPop();
+//    }
+//    // Clear array-backed stack and reserve capacity
+//    arr.clear();
+//    arr.reserve(n);
+//    arrTop = -1;
+//
+//    for (int i = 0; i < n; ++i) {
+//        // Use i directly; avoid invalid expression like i[0]
+//        goodPushLL(i);
+//        goodPushArray(i);
+//    }
+//
+//    std::cout << "Pushed " << n << " elements: array size = " << arr.size() << "\n";
+//
+//    // Cleanup linked list before next iteration
+//    while (top) {
+//        goodPop();
+//    }
+//}
