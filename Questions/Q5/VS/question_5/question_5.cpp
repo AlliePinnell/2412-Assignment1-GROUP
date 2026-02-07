8fe090d747517cf0f964b172c32dfbb2a551f6b7
Remy-Post <166053369+Remy-Post@users.noreply.github.com>
2026-02-07 14:33:43 -0500

    Add files via upload

--- Questions/Q5/VS/question_5/question_5.cpp
+++ Questions/Q5/VS/question_5/question_5.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 
-int recursiveFibonacci(int number) {
-    if (number <= 1) {
-        return number; 
+/*
+    Q5: 
+    1) recursiveFibonacci -> calculates the fibonacci sequence of a given number, 'n', using recursion
+*/
+
+int recursiveFibonacci(int n) {
+    if (n <= 1) {
+        // Base case: fibonacci of 0 is 0, fibonacci of 1 is 1 - Constant - Θ(1)
+        return n; 
 
     } else {
-        return (recursiveFibonacci(number - 1) + recursiveFibonacci(number - 2)  );
+        // Recursive case: fibonacci of n is the sum of the previous two fibonacci numbers - Exponential - Θ(2^n)
+        return (recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2)  );
     }
 }
 
 int main () {
+    // Exponential - Θ(2^n)
     for (int i = 0; i < 7; i++) {
         std::cout << recursiveFibonacci(i) << ", ";
     }
