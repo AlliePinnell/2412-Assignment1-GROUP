0cd0cf21b0f0d9cdd1ce4c95082188c1b888317e
Remy-Post <166053369+Remy-Post@users.noreply.github.com>
2026-02-07 14:32:59 -0500

    Add files via upload

--- Questions/Q4/VS/question_4/question_4.cpp
+++ Questions/Q4/VS/question_4/question_4.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 
-int recursiveFactorial(int number) {
-    if (number == 1) {
+/*
+    Q4: 
+    1) recursiveFactorial -> calculates the factorial of a given number, 'n', using recursion
+*/
+
+// 1 | Recursive Factorial Function
+// Time Complexity: Linear - Θ(n) - [Θ(1) + Θ(n) => Θ(n)]
+int recursiveFactorial(int n) {
+    if (n == 1) {
+        // Base case: factorial of 1 is 1 - Constant - Θ(1)
         return 1;
-    } else {
-        return (number * recursiveFactorial(number-1) );
-    }
+    } 
+    // Recursive case: n! = n * (n-1)! - Linear - Θ(n)
+    return (n * recursiveFactorial(n-1) );
 }
 
 int main () {
     std::cout << recursiveFactorial(5)<< std::endl;
+    return 0;
 }
