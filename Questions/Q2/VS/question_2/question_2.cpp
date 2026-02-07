184052f096d6f1c07d34ccef28234db8a0ffe25e
Remy-Post <166053369+Remy-Post@users.noreply.github.com>
2026-02-07 14:31:50 -0500

    Add files via upload

--- Questions/Q2/VS/question_2/question_2.cpp
+++ Questions/Q2/VS/question_2/question_2.cpp
@@ -3,6 +3,18 @@
 #include <string>
 using namespace std;
 
+
+/* Q2: 
+ 1) reverseLinkedList -> reverses the order of elements in a given singly linked list
+
+ 2) Engage the user
+ 2.1) Insert elements into a linked list
+ 2.2) Traverse and print its elements
+ 2.3) Apply the reverseLinkedList function to reverse the order of the list
+ 2.4) Traverse and print the reversed list
+*/
+
+// Linked List Node Structure | 
 struct node {
     int data;
     struct node* next;
@@ -13,8 +25,13 @@ struct node {
     }
 };
 
+
+// 2.1 | Insert Node Function 
+// Time Complexity: Linear - Θ(n) [Θ(1) + Θ(n) => Θ(n)]
 void insertNode (node*& head, int newData) {
 
+    // Base case: if the list is empty, create a new node
+    // Time Complexity: Constant - Θ(1)
     if (head == nullptr) {
         node* tp = new node(newData);
         head = tp;
@@ -24,7 +41,9 @@ void insertNode (node*& head, int newData) {
 
     node* tp = head;
 
-    while (tp->next) {
+    // Traverse to the end of the list
+    // Time Complexity: Linear -  Θ(n)
+    while (tp->next) { // Θ(n)
         tp = tp->next;
     }
 
@@ -33,20 +52,27 @@ void insertNode (node*& head, int newData) {
     return;
 }
 
+
+// 2.2 | Output Singly Linked List Function 
+// Time Complexity: Θ(n)
 void outputSinglyLinkedList (node*& head) {
     node* tp = head;
-    while (tp) {
+    while (tp) { // Θ(n)
         std::cout << tp->data << " -> ";
         tp = tp->next;
     }
 }
 
+
+// 1 | Reverse Linked List Function 
+// Time Complexity: Θ(n)
 node* reverseLinkedList (node*& head) {
     node* previous = nullptr;
     node* current = head;
     node* next = nullptr;
 
-    while (current) {
+    // Iterate through the list and reverse the links
+    while (current) { // Θ(n)
         next = current->next;
         current->next = previous;
         previous = current;
