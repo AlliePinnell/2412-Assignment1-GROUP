52ccc5b6537ad00fbaaba6113bbb34d21f08f91d
AlliePinnell <x-rap@hotmail.ca>
2026-02-07 18:51:10 -0500

    Update question_9.cpp
    Co-Authored-By: Omasta5 <194012390+Omasta5@users.noreply.github.com>

--- Questions/Q9/VS/question_9/question_9/question_9.cpp
+++ Questions/Q9/VS/question_9/question_9/question_9.cpp
@@ -14,32 +14,27 @@ struct node {
 	}
 };
 
-/*node* createDoublyLinkedList(node* head, node* tail, string line) {
-	node* currentLeft = nullptr;
-	node* currentRight = nullptr;
-	node* prevLeft = nullptr;
-	node* prevRight = nullptr
+void createDoubleLinkedList(node*& head, node*& tail, const std::string& line) {
+	head = nullptr;
+	tail = nullptr;
 
-	for (int i = 0; i < line.size(); i++) {
-		if (head == nullptr && tail == nullptr) {
-			head = new node(line[i]);
-			tail = new node(line[line.size() - 1 - i]);
+	for (char c : line) {
+		if (!std::isalnum(c)) continue;
 
-			currentLeft = head;
-			currentRight = tail;
-			continue;
+		node* newNode = new node(std::tolower(c));
+
+		if (!head) {
+			head = tail = newNode;
 		}
+		else {
+			tail->next = newNode;
+			newNode->prev = tail;
+			tail = newNode;
+		}
+	}
+}
 
-		node* tpLeft = new node(line[i]);
-		node* tpRight = new node(line[line.size() - 1 - i]);
 
-		currentLeft->left = tpLeft;
-		currentRight->right = tpRight;
-
-	}// end for
-}*/
-
-// THIS IMPLEMENTATION CHANGES UP HIS FUNCTION AND DOESN'T USE std::string& str SO MIGHT NOT BE OKAY TO USE
 bool isPalindrome(const std::string& str, node* head, node* tail) {
 	node* currentLeft = head;
 	node* currentRight = tail;
@@ -47,7 +42,7 @@ bool isPalindrome(const std::string& str, node* head, node* tail) {
 	while (currentLeft) {
 
 		std::cout << "Current Left: " << currentLeft->data << std::endl;
-		std::cout << "Current Right: " << currentLeft->data << std::endl;
+		std::cout << "Current Right: " << currentRight->data << std::endl;
 
 		if (currentLeft->data != currentRight->data) {
 			return false;
@@ -64,65 +59,20 @@ using namespace std;
 
 int main() {
 	std::string input;
+	node* head = nullptr;
+	node* tail = nullptr;
 
 	std::cout << "Enter a string: ";
 
-	//std::getline(std::cin, input);
+	std::getline(std::cin, input);
 
-	// DELETE THIS, THIS IS TEST CODE
-
-	// Create the first node (head of the list)
-	node* head = new node('a');
-
-	// Create and link the second node
-	head->next = new node('b');
-	head->next->prev = head;;
-
-	// Create first tail node
-	node* tail = new node('a');
-
-	// Create and link the second node
-	tail->prev = new node('b');
-	tail->prev->next = tail;
-
-	// Link the Create Nodes
-	tail->prev->prev = head->next;
-	head->next->next = tail->prev;
-	// Traverse the list forward and print elements
-	std::cout << "Outputting Through Head" << std::endl;
-	node* temp = head;
-	while (temp != nullptr)
-	{
-		cout << temp->data;
-		if (temp->next != nullptr)
-		{
-			cout << " <-> ";
-		}
-		temp = temp->next;
-	}
-	std::cout << std::endl;
-	std::cout << "Outputting Through Tail" << std::endl;
-	node* temp2 = tail;
-	while (temp2 != nullptr)
-	{
-		cout << temp2->data;
-		if (temp2->prev != nullptr)
-		{
-			cout << " <-> ";
-		}
-		temp2 = temp2->prev;
-
-	}
-	std::cout << std::endl;
+	createDoubleLinkedList(head, tail, input);
 
 	if (isPalindrome(input, head, tail) == true) {
-
 		std::cout << "The entered string is a palindrome.\n";
-
-	} else {
-
+	}
+	else {
 		std::cout << "The entered string is not a palindrome.\n";
-
 	}
 	return 0;
 }
